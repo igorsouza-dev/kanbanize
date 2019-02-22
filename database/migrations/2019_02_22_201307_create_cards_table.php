eb040d7dcfa89092aef1348601f604e801237859
Igor Souza <igorsouza.dev@gmail.com>
2019-02-22 17:26:10 -0300

    Adding migrations

---
+++ database/migrations/2019_02_22_201307_create_cards_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCardsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cards', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('order_id');
+            $table->string('title');
+            $table->string('description');
+            $table->string('color');
+            $table->unsignedInteger('user_id');
+            $table->unsignedInteger('column_id');
+            $table->dateTime('deadline');
+            $table->unsignedInteger('column_position');
+            $table->tinyInteger('type');
+            $table->tinyInteger('priority');
+
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('column_id')->references('id')->on('columns');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cards');
+    }
+}
---
+++ database/migrations/2019_02_22_201333_create_columns_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateColumnsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('columns', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('board_id');
+            $table->unsignedInteger('board_position');
+            $table->unsignedInteger('parent_column_id');
+            $table->string('name');
+            $table->unsignedInteger('max_cards');
+            $table->unsignedInteger('min_cards');
+
+            $table->timestamps();
+            $table->softDeletes();
+            $table->foreign('board_id')->references('id')->on('boards');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('columns');
+    }
+}
